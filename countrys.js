aa0ca89b251a74a1eb6a6f8b760667ff14b35676
dev-rahim <rahimuddin5915@gmail.com>
2022-09-12 01:29:32 +0600

    exploring countrys api

--- countrys.js
+++ countrys.js
@@ -8,13 +8,31 @@ const getcountrys = data => {
     const countrysContainer = document.getElementById('countrys');
     data.forEach(country => {
         const div = document.createElement('div');
-        const h3 = document.createElement('h3');
-        const p = document.createElement('p');
-        h3.innerText = `${country.name.official}`;
-        p.innerText = `${country.capital}`;
-        div.appendChild(h3);
-        div.appendChild(p);
+        div.innerHTML = `
+    <h3>${country.name.official}</h3>
+    <p>${country.capital}</p>
+    <button onclick="getCountryByName('${country.name.common}')">More Details</button>
+        `;
         countrysContainer.appendChild(div);
         div.classList.add('country')
     });
+}
+
+const getCountryByName = (name) => {
+    const url = `https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showCountryDetails(data))
+
+}
+const showCountryDetails = (data) => {
+    const countryDetails = document.getElementById('countryDetails');
+    countryDetails.innerHTML = `
+    <h1>Country Details</h1>
+    <h3>Name:${data[0].name.official}</h3>
+    <img style="text-align:center" width="150px" src="${data[0].flags.png}" alt="">
+    <p>Population:${data[0].population}</p>
+    <p>Area:${data[0].area}</p>
+    `;
+    
 }
--- kanye.js
+++ kanye.js
@@ -3,7 +3,7 @@ const loadQuotes = () => {
         .then(res => res.json())
         .then(data => getQuotes(data))
 }
-
+loadQuotes();
 const getQuotes = (quote) => {
     const quotesContainer = document.getElementById('quotes');
     quotesContainer.innerHTML = `
